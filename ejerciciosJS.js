307456d5f4ef740e3b6bfad16198bd76fc190daf
Andrea <andrea.vargas18@hotmail.es>
2023-05-11 16:48:09 -0500

    Añadiendo ejercicio con includes y filter

--- ejerciciosJS.js
+++ ejerciciosJS.js
@@ -27,3 +27,31 @@ function ordenaLibros (libros){
       { nombre: "La Iliada", publicacion: -750, autor: "Homero"},
       { nombre: "La Odisea", publicacion: -725, autor: "Homero" }
     ])
+
+    //Tenemos dos listas de asistentes a dos cursos: html y css. Queremos obtener una lsita de los asistentes a ambos cursos. 
+    //La lista estará ordenada.//Por ejemplo html = ['Pedro','Juan','Ana'], y 
+    //css=['Luis','Juan', 'Ana','María'] la lista obtenida sería ['Juan', 'Ana'].
+
+    function asistenteIgual(html, css) {
+    let repetidos = html.filter(arreglo => css.includes(arreglo))
+    return repetidos
+    }
+    console.log(asistenteIgual((['Pedro','Juan','Ana']),(['Luis','Juan', 'Ana','María'])))
+
+    //OTRA ALTERNATIVA 
+
+    let aux = []
+let contador = ""
+let html = ['Pedro','Juan','Ana','Luis'];
+let  css = ['Luis','Juan', 'Ana','María'];
+for (let i = 0; i < html.length; i++) {
+  for (let j = 0; j < css.length; j++) {
+if (html[i] == css[j]) {
+  contador = html[i]
+  aux.push(contador)
+}
+  }}
+console.log(aux)
+
+let html1 = ['Pedro','Juan','Ana'];
+let  css1 = ['Luis','Juan', 'Ana','María'];
